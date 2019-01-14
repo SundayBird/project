9490a6bce43afe033af5a4af8837b8310d5d0fb8
Wonyoung Ju <getogrand@gmail.com>
2019-01-15 00:08:33 +0900

    Do not use git submodule

---
+++ 05/swift/NumberBaseball/NumberBaseball/Possibility.swift
@@ -0,0 +1,51 @@
+//
+//  Possibility.swift
+//  NumberBaseball
+//
+//  Created by WonyoungJu on 13/01/2019.
+//  Copyright © 2019 getogrand. All rights reserved.
+//
+
+import Foundation
+
+public struct Possibility {
+    let answer: String
+
+    static func list(from judgements: [Judgement]) -> [Possibility] {
+        var wholePossibilities: [Possibility] {
+            return (111...999).map { (num) -> Possibility in
+                return Possibility(answer: "\(num)")
+            }
+        }
+
+        return wholePossibilities.filter { (possibility) -> Bool in
+            for judgement in judgements {
+                if !possibility.isMatched(with: judgement) {
+                    return false
+                }
+            }
+            return true
+        }
+    }
+
+    func isMatched(with judgement: Judgement) -> Bool {
+        var strikeCount = 0
+        var ballCount = 0
+
+        let answer = self.answer
+        let guess = judgement.guess
+
+        for i in answer.indices {
+            for j in guess.indices {
+                if i == j && answer[i] == guess[j] {
+                    strikeCount += 1
+                }
+                if i != j && answer[i] == guess[j] {
+                    ballCount += 1
+                }
+            }
+        }
+
+        return judgement.strikeCount == strikeCount && judgement.ballCount == ballCount
+    }
+}
---
+++ 05/swift/NumberBaseball/NumberBaseball/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  NumberBaseball
+//
+//  Created by WonyoungJu on 12/01/2019.
+//  Copyright © 2019 getogrand. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
