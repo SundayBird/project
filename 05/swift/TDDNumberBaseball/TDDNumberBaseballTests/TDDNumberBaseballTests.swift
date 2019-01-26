cb22334a74e39fae478c75a63a3308e3f251337e
Wonyoung Ju <getogrand@gmail.com>
2019-01-26 19:25:18 +0900

    Add testcase

--- 05/swift/TDDNumberBaseball/TDDNumberBaseballTests/TDDNumberBaseballTests.swift
+++ 05/swift/TDDNumberBaseball/TDDNumberBaseballTests/TDDNumberBaseballTests.swift
@@ -7,27 +7,19 @@
 //
 
 import XCTest
+@testable import NumberBaseball
 
 class TDDNumberBaseballTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testPossibilityListCase1() {
+        let judgements: [Judgement] = [
+            Judgement(guess: "123", strikeCount: 1, ballCount: 1),
+            Judgement(guess: "356", strikeCount: 1, ballCount: 0),
+            Judgement(guess: "327", strikeCount: 2, ballCount: 0),
+            Judgement(guess: "489", strikeCount: 0, ballCount: 1),
+            ]
+        let possibilities = Possibility.list(from: judgements)
+        XCTAssertEqual(2, possibilities.count)
     }
 
 }
