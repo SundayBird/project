631992173fec6598d5019a1ecd62d49a8184abf5
Wonyoung Ju <getogrand@gmail.com>
2019-01-26 19:31:32 +0900

    Make it right

--- 05/swift/TDDNumberBaseball/TDDNumberBaseball/Possibility.swift
+++ 05/swift/TDDNumberBaseball/TDDNumberBaseball/Possibility.swift
@@ -9,7 +9,43 @@
 import Foundation
 
 struct Possibility {
-    static func list(from judgement: [Judgement]) -> [Possibility] {
-        return [Possibility(), Possibility()]
+    let answer: String
+    
+    static func list(from judgements: [Judgement]) -> [Possibility] {
+        var wholePossibilities: [Possibility] {
+            return (111...999).map { (num) -> Possibility in
+                return Possibility(answer: "\(num)")
+            }
+        }
+
+        return wholePossibilities.filter { (possibility) -> Bool in
+            for judgement in judgements {
+                if !possibility.isMatched(with: judgement) {
+                    return false
+                }
+            }
+            return true
+        }
+    }
+
+    func isMatched(with judgement: Judgement) -> Bool {
+        var strikeCount = 0
+        var ballCount = 0
+
+        let answer = self.answer
+        let guess = judgement.guess
+
+        for i in answer.indices {
+            for j in guess.indices {
+                if i == j && answer[i] == guess[j] {
+                    strikeCount += 1
+                }
+                if i != j && answer[i] == guess[j] {
+                    ballCount += 1
+                }
+            }
+        }
+
+        return judgement.strikeCount == strikeCount && judgement.ballCount == ballCount
     }
 }
