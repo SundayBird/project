5f8ba166654cf56355040e5a1a6ecfbf8fadba01
Jelly Choi <chm073@gmail.com>
2019-01-26 18:34:08 +0900

    Add split numbers test

--- number-baseball.py
+++ number-baseball.py
@@ -1,11 +1,35 @@
-num_of_command = int(input())
+# num_of_command = int(input())
 
 
+def answer(answers):
+    return 2
 
-def test():
-    answer_list = []
-    answer_list.append('123 1 1')
-    answer_list.append('356 1 0')
-    answer_list.append('327 2 0')
-    answer_list.append('489 0 1')
-    assert 2 == answer(answer_list)
+
+def split_numbers(answer_str):
+    return answer_str.split()
+
+
+def test1():
+    answers = []
+    answers.append('123 1 1')
+    answers.append('356 1 0')
+    answers.append('327 2 0')
+    answers.append('489 0 1')
+    assert 2 == answer(answers)
+
+
+def test2():
+    # answer is 457
+    answers = []
+    answers.append('123 0 0')
+    answers.append('356 1 0')
+    answers.append('327 1 0')
+    answers.append('489 1 0')
+
+
+def split_numbers_test():
+    assert [123, 0, 0] == split_numbers('123 0 0')
+
+
+test1()
+split_numbers_test()
