a8991d1524ea9fed6bd0e0e18682e76f93e7b144
Wonyoung Ju <getogrand@gmail.com>
2019-01-26 19:30:27 +0900

    Make test passes

---
+++ 05/swift/TDDNumberBaseball/TDDNumberBaseball/Possibility.swift
@@ -0,0 +1,15 @@
+//
+//  Possibility.swift
+//  TDDNumberBaseball
+//
+//  Created by WonyoungJu on 26/01/2019.
+//  Copyright © 2019 getogrand. All rights reserved.
+//
+
+import Foundation
+
+struct Possibility {
+    static func list(from judgement: [Judgement]) -> [Possibility] {
+        return [Possibility(), Possibility()]
+    }
+}
--- 05/swift/TDDNumberBaseball/TDDNumberBaseballTests/TDDNumberBaseballTests.swift
+++ 05/swift/TDDNumberBaseball/TDDNumberBaseballTests/TDDNumberBaseballTests.swift
@@ -7,7 +7,7 @@
 //
 
 import XCTest
-@testable import NumberBaseball
+@testable import TDDNumberBaseball
 
 class TDDNumberBaseballTests: XCTestCase {
 
