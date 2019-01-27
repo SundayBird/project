29f17dff108fe9eb2f762b12dbd4fc9b3c3e812c
Joshua kim <joshuakim.changer@gmail.com>
2019-01-27 09:56:48 +0900

    Refactor

--- 04/typescript/src/index.ts
+++ 04/typescript/src/index.ts
@@ -1,9 +1,19 @@
 import assert from 'assert';
 
-const verifyGuess = (a: string, b: string) => ({
-  strike: 3,
-  ball: 0,
-});
+const verifyGuess = (guess: string, actual: string) => {
+  const strike = Array.from(guess).reduce((acc, curr, idx) => {
+    if (curr === actual[idx]) {
+      return acc + 1;
+    } else {
+      return acc;
+    }
+  }, 0);
+  const ball = 0;
+  return {
+    strike,
+    ball,
+  }
+}
 
 assert.deepEqual(verifyGuess("123", "123"), {
   strike: 3,
