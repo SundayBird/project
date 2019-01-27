ff3f8e3833f88e482ae864068ef3b470aa6e4e7a
Joshua kim <joshuakim.changer@gmail.com>
2019-01-27 09:51:16 +0900

    Make a Little Change: Implement verifyGuess

--- 04/typescript/src/index.ts
+++ 04/typescript/src/index.ts
@@ -1,5 +1,10 @@
 import assert from 'assert';
 
+const verifyGuess = (a: string, b: string) => ({
+  strike: 3,
+  ball: 0,
+});
+
 assert.deepEqual(verifyGuess("123", "123"), {
   strike: 3,
   ball: 0,
